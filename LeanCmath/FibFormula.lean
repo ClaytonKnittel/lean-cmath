d479bbe4b0d66de06a42bf05a92570addf5360ac
Clayton Knittel <cknit1999@gmail.com>
2025-05-02 23:07:59 -0700

    Simplify further.

--- LeanCmath/FibFormula.lean
+++ LeanCmath/FibFormula.lean
@@ -4,20 +4,20 @@ import Mathlib.Data.Nat.Fib.Basic
 import Mathlib.Tactic.Linarith
 import Mathlib.Data.Real.Sqrt
 
-noncomputable def φ : Real := (√5 + 1) / 2
-noncomputable def φ₂ : Real := (√5 - 1) / 2
+noncomputable def φ : Real := (1 + √5) / 2
+noncomputable def φ₂ : Real := (1 - √5) / 2
 
 theorem phi_sq : φ + 1 = φ * φ := by
   unfold φ
   field_simp [mul_add, add_mul]
   linarith
 
-theorem phi2_sq : φ₂ - 1 = -φ₂ * φ₂ := by
+theorem phi2_sq : φ₂ + 1 = φ₂ * φ₂ := by
   unfold φ₂
   field_simp [sub_mul, mul_sub]
   linarith
 
-theorem fib_formula : ∀ n, Nat.fib n = (φ ^ n - (-φ₂) ^ n) / Real.sqrt 5 := by
+theorem fib_formula : ∀ n, Nat.fib n = (φ ^ n - φ₂ ^ n) / Real.sqrt 5 := by
   intro n
   cases n with
   | zero => simp
@@ -26,20 +26,14 @@ theorem fib_formula : ∀ n, Nat.fib n = (φ ^ n - (-φ₂) ^ n) / Real.sqrt 5 :
     | zero =>
       unfold φ φ₂
       field_simp [fib_formula Nat.zero]
-      linarith
     | succ n =>
-      field_simp [Nat.fib_add_two, fib_formula n, fib_formula (n + 1)]
-      rw [neg_eq_neg_one_mul φ₂]
-      repeat rw [mul_pow]
-      field_simp [pow_add _ n 1, mul_comm]
+      field_simp [Nat.fib_add_two, fib_formula n, fib_formula (n + 1), pow_add _ n 1, mul_comm]
       calc
-        _ = (φ + 1) * φ ^ n + (φ₂ - 1) * (φ₂) ^ n * (-1) ^ n := ?_
-        _ = φ ^ (n + 2) - (φ₂) ^ (n + 2) * (-1) ^ (n + 2) := ?_
+        _ = (φ + 1) * φ ^ n - (φ₂ + 1) * (φ₂) ^ n := ?_
+        _ = φ ^ (n + 2) - (φ₂) ^ (n + 2) := ?_
       . linarith
       . have sq : ∀ (n : ℝ), n * n = n ^ 2 := by
           intro n
           rw [← pow_one n, ← pow_add]
           field_simp
         field_simp [phi_sq, phi2_sq, sq φ, sq φ₂, ← pow_add, add_comm 2, Mathlib.Tactic.RingNF.add_neg]
-        left
-        field_simp [pow_add]
