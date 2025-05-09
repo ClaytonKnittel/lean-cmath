7e2b59dd71c697c4c789ee4eb9bc8dd83d7f6409
Clayton Knittel <cknit1999@gmail.com>
2025-05-09 01:53:43 -0700

    Fix build failure

---
+++ LeanCmath/Basic.lean
@@ -0,0 +1,4 @@
+import LeanCmath.FibFormula
+import LeanCmath.Imo2023p1
+import LeanCmath.InfPrimes
+import LeanCmath.PrimePower
--- LeanCmath/PrimePower.lean
+++ LeanCmath/PrimePower.lean
@@ -4,6 +4,9 @@ import Mathlib.Data.Nat.Factorization.Basic
 import Mathlib.Data.Nat.Factors
 import Mathlib.Tactic.Linarith
 
+open Finsupp (single single_eq_same single_eq_of_ne)
+open Nat (eq_of_factorization_eq eq_zero_of_le_zero factorization_le_iff_dvd pow_pos Prime)
+
 def PrimePowerExp (p exp n : ℕ) := p.Prime ∧ p ^ exp = n
 def PrimePower (p n : ℕ) := ∃ exp, PrimePowerExp p exp n
 def IsPrimePower (n : ℕ) := ∃ p, PrimePower p n
@@ -18,26 +21,26 @@ namespace PrimePower
 
 theorem gt_zero {n : ℕ} (hn : IsPrimePower n) : 0 < n := by
   let ⟨_, ⟨_, ⟨p_prime, h⟩⟩⟩ := hn
-  exact h.symm ▸ Nat.pow_pos p_prime.pos
+  exact h.symm ▸ pow_pos p_prime.pos
 
 theorem ne_zero {n : ℕ} (hn : IsPrimePower n) : n ≠ 0 :=
   (gt_zero hn).ne'
 
 theorem one_factor {n p e : ℕ} (n_ne_0 : n ≠ 0) (p_prime : p.Prime) :
-    PrimePowerExp p e n ↔ n.factorization = Finsupp.single p e :=
+    PrimePowerExp p e n ↔ n.factorization = single p e :=
   ⟨
-    fun ⟨_, h⟩ => h ▸ Nat.Prime.factorization_pow p_prime,
-    fun h : n.factorization = (Finsupp.single p e) => by
+    fun ⟨_, h⟩ => h ▸ Prime.factorization_pow p_prime,
+    fun h : n.factorization = (single p e) => by
       have : n = p ^ e :=
-        Nat.eq_of_factorization_eq
+        eq_of_factorization_eq
           n_ne_0
           (pow_ne_zero _ p_prime.ne_zero)
-          (fun q => congrArg (· q) (h ▸ (Nat.Prime.factorization_pow p_prime).symm))
+          (fun q => congrArg (· q) (h ▸ (Prime.factorization_pow p_prime).symm))
       exact ⟨p_prime, this.symm⟩
   ⟩
 
 theorem pp_fact_is_single {n p e : ℕ} (hn : PrimePowerExp p e n)
-    : n.factorization = Finsupp.single p e :=
+    : n.factorization = single p e :=
   let ⟨pp, _⟩ := hn
   ((one_factor ∘ ne_zero ∘ ppe_is_pp) hn pp).mp hn
 
@@ -46,7 +49,7 @@ theorem dvd_is_pp {n p m : ℕ} (hn : PrimePower p n) (h : m ∣ n)
   let ⟨e, ⟨p_prime, n_eq_p_exp⟩⟩ := hn
   let n_ne_0 := ne_zero (pp_is_pp hn)
   let m_ne_0 := ne_zero_of_dvd_ne_zero n_ne_0 h
-  let le_fact := (Nat.factorization_le_iff_dvd m_ne_0 n_ne_0).mpr h
+  let le_fact := (factorization_le_iff_dvd m_ne_0 n_ne_0).mpr h
   let m_exp := m.factorization p
   let n_fact := ((one_factor n_ne_0 p_prime).mp ⟨p_prime, n_eq_p_exp⟩)
 
@@ -54,14 +57,13 @@ theorem dvd_is_pp {n p m : ℕ} (hn : PrimePower p n) (h : m ∣ n)
     fun q h =>
       Eq.trans
         (congrArg (· q) n_fact)
-        (Finsupp.single_eq_of_ne h.symm)
-  let m_fact_eq : m.factorization = Finsupp.single p m_exp :=
+        (single_eq_of_ne h.symm)
+  let m_fact_eq : m.factorization = single p m_exp :=
     Finsupp.ext fun q =>
       if h : p = q then
-        h ▸ Finsupp.single_eq_same.symm
+        h ▸ single_eq_same.symm
       else
-        (Finsupp.single_eq_of_ne h : (Finsupp.single p m_exp) q = 0).symm ▸
-          Nat.eq_zero_of_le_zero
-            (n_fact_0 q (h ∘ Eq.symm) ▸ le_fact q)
+        (single_eq_of_ne h : (single p m_exp) q = 0).symm ▸
+          eq_zero_of_le_zero (n_fact_0 q (h ∘ Eq.symm) ▸ le_fact q)
 
   exact ⟨m_exp, (one_factor m_ne_0 p_prime).mpr m_fact_eq⟩
