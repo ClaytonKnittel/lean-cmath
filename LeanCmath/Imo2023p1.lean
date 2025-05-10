a1f54bfee1bd2449bf578d52e7d6673f66d7dd7b
Clayton Knittel <cknit1999@gmail.com>
2025-05-10 12:54:26 -0700

    Use mathlib prime power theorems

--- LeanCmath/Imo2023p1.lean
+++ LeanCmath/Imo2023p1.lean
@@ -1,7 +1,7 @@
+import Mathlib.Algebra.IsPrimePow
 import Mathlib.Data.Nat.Prime.Defs
 import Mathlib.Data.Nat.Factors
 import Mathlib.Tactic.Linarith
-import LeanCmath.PrimePower
 
 def ConsecutiveFactors (n a b : ℕ) :=
   a ∣ n ∧ b ∣ n ∧ a < b ∧ ¬∃ c, (c ∣ n ∧ a < c ∧ c < b)
@@ -18,12 +18,13 @@ def Dividable (n : ℕ) :=
     ConsecutiveFactors n a b ∧ ConsecutiveFactors n b c
     → a ∣ b + c
 
-lemma cons_are_p_apart {x y p n : ℕ} (hn : PrimePower p n)
+lemma cons_are_p_apart {x y p n : ℕ} (p_prime : p.Prime)
+    (hn : ∃ k, 0 < k ∧ p ^ k = n)
     : ConsecutiveFactors n x y → x * p = y := by
-  let ⟨e, ⟨p_prime, n_eq_p_exp⟩⟩ := hn
+  let ⟨e, ⟨e_gt_0, n_eq_p_exp⟩⟩ := hn
   intro ⟨hx, ⟨hy, ⟨x_lt_y, h⟩⟩⟩
-  let ⟨x_exp, ⟨_, x_eq_p_exp⟩⟩ := PrimePower.dvd_is_pp hn hx
-  let ⟨y_exp, ⟨_, y_eq_p_exp⟩⟩ := PrimePower.dvd_is_pp hn hy
+  let ⟨x_exp, ⟨_, x_eq_p_exp⟩⟩ := (Nat.dvd_prime_pow p_prime).mp (n_eq_p_exp ▸ hx)
+  let ⟨y_exp, ⟨_, y_eq_p_exp⟩⟩ := (Nat.dvd_prime_pow p_prime).mp (n_eq_p_exp ▸ hy)
 
   have exp_lt : x_exp < y_exp :=
     (Nat.pow_lt_pow_iff_right p_prime.one_lt).mp
@@ -43,22 +44,22 @@ lemma cons_are_p_apart {x y p n : ℕ} (hn : PrimePower p n)
     let ⟨b, n_eq⟩ := hy
     let ⟨k, y_eq⟩ := Nat.exists_eq_add_of_lt lt
     exists b * p ^ (k + 1)
-    rw [n_eq, ← y_eq_p_exp, y_eq, add_assoc, Nat.pow_add, mul_assoc,
+    rw [n_eq, y_eq_p_exp, y_eq, add_assoc, Nat.pow_add, mul_assoc,
         mul_comm _ b]
 
-  rw [← x_eq_p_exp, ← y_eq_p_exp, ← exp_eq]
+  rw [x_eq_p_exp, y_eq_p_exp, ← exp_eq]
   exact rfl
 
-lemma pp_is_dividable {n : ℕ} : IsPrimePower n → Dividable n :=
-  fun ⟨p, hp⟩ _ _ _ ⟨xy_cons, yz_cons⟩ => by
-    let y_subs := cons_are_p_apart hp xy_cons
-    let z_subs := cons_are_p_apart hp yz_cons
+lemma pp_is_dividable {n : ℕ} : IsPrimePow n → Dividable n :=
+  fun ⟨p, e, hp, hpp⟩ _ _ _ ⟨xy_cons, yz_cons⟩ => by
+    let y_subs := cons_are_p_apart hp.nat_prime ⟨e, hpp⟩ xy_cons
+    let z_subs := cons_are_p_apart hp.nat_prime ⟨e, hpp⟩ yz_cons
     rw [← z_subs, ← y_subs]
     exists p + p ^ 2
     linarith
 
-lemma dividable_is_pp {n : ℕ} (h : n > 1) : Dividable n → IsPrimePower n := by
+lemma dividable_is_pp {n : ℕ} (h : n > 1) : Dividable n → IsPrimePow n := by
   sorry
 
-theorem P1 : ∀ n > 1, IsPrimePower n ↔ Dividable n :=
+theorem P1 : ∀ n > 1, IsPrimePow n ↔ Dividable n :=
   fun _ h => ⟨pp_is_dividable, dividable_is_pp h⟩
